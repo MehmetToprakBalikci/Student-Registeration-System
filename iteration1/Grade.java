d065956cd47933b7d475f0d19f944f6418e6f0e4
Bora Duman <47176909+rabbidknight@users.noreply.github.com>
2023-11-14 23:16:44 +0300

    Grade.java code base
    Assigned to Enes.

---
+++ iteration1/Grade.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+// Grade class
+class Grade {
+    private int numericalGrade;
+
+    // Convert numerical grade to letter grade
+    public String convertGradeToLetter(String numericalGrade) {
+        // This needs to be implemented based on your grading system
+        return "";
+    }
+}
