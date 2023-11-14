72213d059da7a8f96e00de45e8fa3dc999a616bc
Bora Duman <47176909+rabbidknight@users.noreply.github.com>
2023-11-14 23:20:24 +0300

    Advisor.java class code base
    Assigned to Arda.

---
+++ iteration1/Advisor.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+// Advisor class
+class Advisor extends Person {
+    // Method to create advisor
+    public static Advisor create(String name, String lastName) {
+        return new Advisor(name, lastName);
+    }
+
+    // Constructor
+    private Advisor(String name, String lastName) {
+        super(name, lastName);
+    }
+
+    // Get advisor information as a String
+    public String getAdvisorString() {
+        // Implementation needed
+        return "Advisor: " + this.name + " " + this.lastName;
+    }
+
+    // Placeholder for starting advisor actions
+    public void startActions() {
+        // Implementation needed
+    }
+}
