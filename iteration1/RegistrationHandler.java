3f7d825f5a64229cfabb06a9698c9b49f7266d5e
Bora Duman <47176909+rabbidknight@users.noreply.github.com>
2023-11-14 23:22:29 +0300

    RegistrationHandler.java
    Assigned to Bora.

---
+++ iteration1/RegistrationHandler.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+// RegistrationHandler class
+class RegistrationHandler {
+    // Start the course registration process
+    public void start() {
+        // Implementation needed
+    }
+}
