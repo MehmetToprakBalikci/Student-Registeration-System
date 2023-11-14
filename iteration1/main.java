300a8c45d6f1a6e50d229db71afb929968532d78
Bora Duman <47176909+rabbidknight@users.noreply.github.com>
2023-11-14 23:28:01 +0300

    main.java

---
+++ iteration1/main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+// Main class
+public class Main {
+    public static void main(String[] args) {
+        UI ui = UI.create();
+        // Start the UI
+    }
+}
