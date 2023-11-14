673dd2363025c477843cda1574b88240ed4ec3f5
Bora Duman <47176909+rabbidknight@users.noreply.github.com>
2023-11-14 23:19:09 +0300

    CourseSection.java class base code.
    Assigned to Enes.

---
+++ iteration1/CourseSection.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+// CourseSection class
+class CourseSection {
+    private Course course;
+    private int sectionNumber;
+
+    // Constructor
+    public CourseSection(Course course, int sectionNumber) {
+        this.course = course;
+        this.sectionNumber = sectionNumber;
+    }
+}
