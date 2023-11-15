fd14505bad7ac5e365871d4957c643bd93124fac
toprakbalikci <149236560+MehmetToprakBalikci@users.noreply.github.com>
2023-11-15 18:44:31 +0300

    Constructor Draft

--- iteration1/Transcript.java
+++ iteration1/Transcript.java
@@ -1,5 +1,17 @@
-public class Transcript {
-    private Transcript(Transcript transcript) {
+import java.util.List;
 
+public class Transcript {
+
+    //Copy constructor,
+    //Constructor to be used with an already existing Transcript Object
+    private Transcript(Transcript transcript) {
+        this.listOfCourses = transcript.listOfCourses;
+        this.listOfGrades = transcript.listOfGrades;
+        this.studentCredits = transcript.studentCredits;
     }
+
+    private List<Course> listOfCourses;
+    private List<Grade> listOfGrades;
+    private int studentCredits;
+
 }
