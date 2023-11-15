7cfeea142ff6163e72c59edd8e175da4417ec6db
toprakbalikci <149236560+MehmetToprakBalikci@users.noreply.github.com>
2023-11-15 18:56:32 +0300

    Methods' Implementation Left

--- iteration1/Transcript.java
+++ iteration1/Transcript.java
@@ -1,5 +1,7 @@
 import java.util.List;
 
+//Transcript class for each student
+//Keeps course list and grade list
 public class Transcript {
 
     //Copy constructor,
@@ -10,8 +12,25 @@ public class Transcript {
         this.studentCredits = transcript.studentCredits;
     }
 
+    //Constructor with parameters
+    private Transcript(List<Course> listOfCourses, List<Grade> listOfGrades, int studentCredits) {
+        this.listOfCourses = listOfCourses;
+        this.listOfGrades = listOfGrades;
+        this.studentCredits = studentCredits;
+    }
+
     private List<Course> listOfCourses;
     private List<Grade> listOfGrades;
     private int studentCredits;
+
+
+    //TODO
+    public void calculateTranscriptFromCourseInside() {
+
+    }
+    //TODO
+    public void getTranscriptString() {
+
+    }
 
 }
