ccbc19adc78bc32a4e2b0de850c048bf0e429211
Jedsam <fgokgoz02@hotmail.com>
2023-11-17 02:09:31 +0300

    renamed student test

--- iteration1/TestStudent.java
+++ iteration1/TestStudent.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class StudentTest {
+public class TestStudent {
     static FileWriter testOutputFile;
     public static void main(String args[]){
         try {
