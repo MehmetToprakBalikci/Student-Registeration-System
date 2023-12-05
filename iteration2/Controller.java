2f2a358c05b51933a6345cafbc5a1958e68b3ba1
Arda Öztürk <143508784+ArdaOzturk192@users.noreply.github.com>
2023-12-05 14:42:59 +0300

    Final variable names replaced with all uppercase names

--- iteration2/Controller.java
+++ iteration2/Controller.java
@@ -2,19 +2,19 @@ import java.util.*;
 
 // Controller class
 class Controller {
-    private final UniversityFileSystem UNIVERSITYFILESYSTEM;
+    private final UniversityFileSystem UNIVERSITY_FILE_SYSTEM;
     private jsonWriter jsonWriter;
     private final UI UI;
     private Person person;
     //Default Constructor
     public Controller() {
-    	UNIVERSITYFILESYSTEM = new UniversityFileSystem();
+    	UNIVERSITY_FILE_SYSTEM = new UniversityFileSystem();
     	UI = new UI();
         UI.initialize();
     }
     //Constructor
     public Controller(Scanner input) {
-    	UNIVERSITYFILESYSTEM = new UniversityFileSystem();
+    	UNIVERSITY_FILE_SYSTEM = new UniversityFileSystem();
     	UI = new UI(input);
         UI.initialize();
     }
@@ -23,10 +23,10 @@ class Controller {
 
     public void start() {
         // load all json course and person files
-    	UNIVERSITYFILESYSTEM.loadFiles();
+    	UNIVERSITY_FILE_SYSTEM.loadFiles();
         do {
             String[] userInfo = UI.requestCredentials();
-            person = UNIVERSITYFILESYSTEM.getSignedPerson(userInfo, this);
+            person = UNIVERSITY_FILE_SYSTEM.getSignedPerson(userInfo, this);
         } while (person == null);
         person.startActions(this);
         jsonWriter = new jsonWriter(person);
