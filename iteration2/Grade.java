58d3ea8aae54e8e58af29048dc9574d8799663ab
Arda Öztürk <143508784+ArdaOzturk192@users.noreply.github.com>
2023-12-06 15:44:52 +0300

    Final variable name replaced with all uppercase name

--- iteration2/Grade.java
+++ iteration2/Grade.java
@@ -3,38 +3,38 @@
 
 // Grade class
 class Grade {
-    private final int numericalGrade;
+    private final int NUMERICAL_GRADE;
 
     // Contructor
     public Grade(int numericalGrade) {
-        this.numericalGrade = numericalGrade;
+        this.NUMERICAL_GRADE = numericalGrade;
     }
 
     public boolean isRetakableGrade() {
-        return numericalGrade < 50;
+        return NUMERICAL_GRADE < 50;
     }
 
     public boolean isPassedGrade() {
-        return numericalGrade >= 35;
+        return NUMERICAL_GRADE >= 35;
     }
 
     // Convert numerical grade to letter grade
     public String getGradeLetter() {
-        if (numericalGrade >= 90 && numericalGrade <= 100) {
+        if (NUMERICAL_GRADE >= 90 && NUMERICAL_GRADE <= 100) {
             return "AA";
-        } else if (numericalGrade >= 80 && numericalGrade < 90) {
+        } else if (NUMERICAL_GRADE >= 80 && NUMERICAL_GRADE < 90) {
             return "BA";
-        } else if (numericalGrade >= 70 && numericalGrade < 80) {
+        } else if (NUMERICAL_GRADE >= 70 && NUMERICAL_GRADE < 80) {
             return "BB";
-        } else if (numericalGrade >= 60 && numericalGrade < 70) {
+        } else if (NUMERICAL_GRADE >= 60 && NUMERICAL_GRADE < 70) {
             return "CB";
-        } else if (numericalGrade >= 50 && numericalGrade < 60) {
+        } else if (NUMERICAL_GRADE >= 50 && NUMERICAL_GRADE < 60) {
             return "CC";
-        } else if (numericalGrade >= 40 && numericalGrade < 50) {
+        } else if (NUMERICAL_GRADE >= 40 && NUMERICAL_GRADE < 50) {
             return "DC";
-        } else if (numericalGrade >= 35 && numericalGrade < 40) {
+        } else if (NUMERICAL_GRADE >= 35 && NUMERICAL_GRADE < 40) {
             return "DD";
-        } else if (numericalGrade < 35 && numericalGrade >= 0) {
+        } else if (NUMERICAL_GRADE < 35 && NUMERICAL_GRADE >= 0) {
             return "FF";
         }
 
@@ -43,10 +43,10 @@ class Grade {
 
     @Override
     public String toString() {
-        return "Grade : " + numericalGrade + ", Letter Grade : " + getGradeLetter();
+        return "Grade : " + NUMERICAL_GRADE + ", Letter Grade : " + getGradeLetter();
     }
 
     public int getNumericalGrade() {
-        return numericalGrade;
+        return NUMERICAL_GRADE;
     }
 }
