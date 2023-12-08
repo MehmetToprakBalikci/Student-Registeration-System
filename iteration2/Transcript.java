77ab6867b018e3d57c833f3282427923b15744af
Arda Öztürk <143508784+ArdaOzturk192@users.noreply.github.com>
2023-12-08 16:42:05 +0300

    Update on student credits for years

--- iteration2/Transcript.java
+++ iteration2/Transcript.java
@@ -48,11 +48,11 @@ public class Transcript {
 
     private int calculateYear() {
         int year = 1;
-        if (studentCredits > 150) {
+        if (studentCredits >= 180) {
             year = 4;
-        } else if (studentCredits > 90) {
+        } else if (studentCredits >= 120) {
             year = 3;
-        } else if (studentCredits > 30) {
+        } else if (studentCredits >= 60) {
             year = 2;
         }
         if (year != 4 && GPA >= 3) {
