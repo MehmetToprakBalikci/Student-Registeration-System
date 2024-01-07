1bd3e87fb4fce9e564b02ad2df38b9c2d3996912
Enes Torluoğlu <110379405+EnesTorluoglu@users.noreply.github.com>
2024-01-07 08:28:10 +0300

    test classes

--- iteration3/testClasses/test_Course.py
+++ iteration3/testClasses/test_Course.py
@@ -20,9 +20,19 @@ class TestCourse(unittest.TestCase):
         self.course4 = Course("CS104", "Principal of Programming Languages", 3, 4, 4, 5, self.lecturer3, self.assistant1, self.pre_requisites3, 65)
         self.course5 = Course("CS105", "Systems Programming", 4, 4, 2, 8, self.lecturer1, self.assistant1, self.pre_requisites1, 50)
 
-        self.tech_elective_course1 = Course("TE101", "Technical Elective 1", 3, 1, 3, 5, self.lecturer1, self.assistant1, [],10)
-        self.tech_elective_course2 = Course("TE102", "Technical Elective 2", 3, 1, 3, 5, self.lecturer2, self.assistant1, [],10)
-        self.tech_elective_course3 = Course("TE103", "Technical Elective 3", 3, 1, 3, 5, self.lecturer3, self.assistant1, [],10)
+        self.tech_elective_course1 = Course("TE101", "Technical Elective 1", 3, 1, 3, 5, self.lecturer1, [],self.assistant1, 10)
+        self.tech_elective_course2 = Course("TE102", "Technical Elective 2", 1, 1, 3, 5, self.lecturer2,  [], self.assistant1,10)
+        self.tech_elective_course3 = Course("TE103", "Technical Elective 3", 3, 2, 3, 5, self.lecturer3, [], self.assistant1,10)
+
+        self.non_tech_elective_course1 = Course("NTE101", "NonTechnical Elective 1", 3, 1, 3, 5, self.lecturer1, [], self.assistant1, 10)
+        self.non_tech_elective_course2 = Course("NTE102", "NonTechnical Elective 2", 1, 1, 3, 5, self.lecturer2, [], self.assistant1, 10)
+        self.non_tech_elective_course3 = Course("NTE103", "NonTechnical Elective 3", 3, 2, 3, 5, self.lecturer3, [],self.assistant1, 10)
+        self.non_tech_elective_course4 = Course("NTE104", "NonTechnical Elective 4", 5, 2, 4, 6, self.lecturer2, [],self.assistant1, 10)
+
+        self.non_tech_elective_course1.set_type("nt")
+        self.non_tech_elective_course2.set_type("nt")
+        self.non_tech_elective_course3.set_type("nt")
+        self.non_tech_elective_course4.set_type("nt")
 
         self.courses1 = []
         self.courses2 = []
@@ -48,12 +58,22 @@ class TestCourse(unittest.TestCase):
         self.assertFalse(self.course2.check_prerequisite(self.courses1, grades1))
 
 
-    def test_check_technical_elective_count_within_threshold(self):
+    def test_check_technical_elective_count(self):
         self.courses1.extend([self.tech_elective_course1, self.tech_elective_course2])
 
         result = self.course1.check_technical_elective_count([], self.courses1, [])
 
         self.assertIsNone(result)
+
+    def test_check_non_technical_elective_count(self):
+        self.courses2.extend([self.non_tech_elective_course1, self.non_tech_elective_course2, self.non_tech_elective_course3, self.non_tech_elective_course4])
+
+        result = self.course1.check_non_technical_elective_count(self.courses2, self.courses1, [])
+
+        expected_message = "This course exceeds the Non-Technical elective course count." \
+                           " Try cancelling another Non-Technical elective or waiting for the cancellation to be accepted."
+
+        self.assertEqual(result, expected_message)
 
 if __name__ == '__main__':
     unittest.main()
--- iteration3/testClasses/test_grade.py
+++ iteration3/testClasses/test_grade.py
@@ -7,36 +7,29 @@ class TestGrade(unittest.TestCase):
         grade1 = Grade(45)
         self.assertTrue(grade1.is_retakable_grade())
 
+    def test_is_not_retakable_grade(self):
         grade2 = Grade(60)
         self.assertFalse(grade2.is_retakable_grade())
 
-    def test_is_passed_grade(self):
+    def test_is_not_passed_grade(self):
         grade1 = Grade(30)
-        self.assertTrue(grade1.is_passed_grade())
+        self.assertFalse(grade1.is_passed_grade())
 
+    def test_is_passed_grade(self):
         grade2 = Grade(40)
-        self.assertFalse(grade2.is_passed_grade())
+        self.assertTrue(grade2.is_passed_grade())
 
-    def test_get_letter_grade(self):
+    def test_get_letter_grade_AA(self):
         grade1 = Grade(95)
         self.assertEqual(grade1.get_letter_grade(), "AA")
 
-        grade2 = Grade(75)
-        self.assertEqual(grade2.get_letter_grade(), "BB")
-
-        grade3 = Grade(45)
-        self.assertEqual(grade3.get_letter_grade(), "DC")
-
+    def test_get_letter_grade_FF(self):
         grade4 = Grade(20)
         self.assertEqual(grade4.get_letter_grade(), "FF")
 
     def test_get_invalid_grade(self):
         grade5 = Grade(105)
         self.assertEqual(grade5.get_letter_grade(), "Invalid Grade")
-
-    def test_get_numerical_grade(self):
-        grade = Grade(85)
-        self.assertEqual(grade.get_numerical_grade(), 85)
 
 
 if __name__ == '__main__':
